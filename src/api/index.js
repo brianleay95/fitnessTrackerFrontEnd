6db4d4b039d5c495b01eed9d93c4c7b0d257c7a8
Eric Flores <ericfullstack@gmail.com>
2021-11-06 11:51:36 -0700

    start activities

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
+import { getToken } from "../auth";
 
-const BASE = 'http://fitnesstrac-kr.herokuapp.com/'
+const BASE = 'http://fitnesstrac-kr.herokuapp.com/api';
 
-export async function getUsers() {
-  try {
-    const { data } = await axios.get(`${ BASE }/users`);
-    return data;
-  } catch (error) {
-    throw error;
-  }
+//Update to allActivities
+export async function allActivities(){
+  try{
+  const { data } = await axios.get(`${ BASE }/activities`, {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+    
+  });
+  // console.log("ALL ACTIVITIES", data)
+return data;
+
+}catch(error){
+  throw error;
+}
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
+  Link
 } from "react-router-dom";
 
-import {Header} from "./components";
+import {
+Header,
+} from "./components";
+
+import{allActivities} from "./api"
+
+import getToken from "./auth";
+import Activities from "./components/Activities";
 
 const App = () => {
   return (
     <div id="App">
-      <h1>Hello, World</h1>
+      {/* <h1>Hello, World</h1> */}
       <Header />
+      
+      <Switch>
+        <Route path="/Activities">
+          <Activities
+          allActivities={allActivities}
+          />
+        </Route>
+      </Switch>
     </div>
   );
+
+
 };
+
+
 
 ReactDOM.render(
   <Router>
